bd65850667642fa8626eeb1c79b63b0d9b32d21f
Kelly <kellysue3@yahoo.com>
2022-08-16 14:02:58 -0600

    initial commit

---
+++ src/models/Image.js
@@ -0,0 +1,11 @@
+export class Image {
+    constructor(data) {
+        this.id = data.id
+        this.date = data.date
+        this.explanation = data.explanation
+        this.mediaType = data.media_type
+        this.title = data.title
+        this.ImgUrl = data.hdurl
+    }
+
+}
